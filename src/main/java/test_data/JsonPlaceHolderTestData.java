483c799c6977fd39df9c52e67140c6ac34805e9b
Linastraa <slmlm211@gmail.com>
2024-04-23 18:52:33 +0300

    adding extra classes

--- src/main/java/test_data/JsonPlaceHolderTestData.java
+++ src/main/java/test_data/JsonPlaceHolderTestData.java
@@ -9,9 +9,15 @@ public class JsonPlaceHolderTestData {
     public static Map<String, Object> expectedDataMap(Integer userId, String title, Boolean completed) {
 
         Map<String, Object> expectedData = new HashMap<>();
-        expectedData.put("userId", userId);
-        expectedData.put("title", title);
-        expectedData.put("completed", completed);
+        if (userId!=null){//If we use null argument for userId, it will not be assign in map body.
+            expectedData.put("userId", userId);
+        }
+        if (title!=null){
+            expectedData.put("title", title);
+        }
+        if (completed!=null){
+            expectedData.put("completed", completed);
+        }
 
         return expectedData;
 
