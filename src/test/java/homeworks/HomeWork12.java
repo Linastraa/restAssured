b151b6c98658e5b73e8b7afefe92a23bfa054607
Linastraa <slmlm211@gmail.com>
2024-04-27 21:43:32 +0300

    solving homework 12

---
+++ src/test/java/homeworks/HomeWork12.java
@@ -0,0 +1,12 @@
+package homeworks;
+
+import base_urls.HomeWork12BaseUrl;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+import pojos.JsonPlaceHolderPojo_HomeWork12;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+public class HomeWork12 extends HomeWork12BaseUrl{
+}
